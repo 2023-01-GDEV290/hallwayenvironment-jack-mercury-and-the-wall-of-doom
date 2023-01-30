8af476158b87ee47a4150474b2d530a852da93e5
LaneAckerman <71029213+LaneAckerman@users.noreply.github.com>
2023-01-30 00:20:48 -0500

    Added floor texture finished hud setup and made progress on wall script

--- Assets/Scripts/wallBreak.cs
+++ Assets/Scripts/wallBreak.cs
@@ -1,11 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class wallBreak: MonoBehaviour
 {
-
-    public int hP = 3;
+    public Animator animator;
+    public int hp = 3;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,8 +22,22 @@ public class wallBreak: MonoBehaviour
     void OnTriggerStay(Collider other)
     {
         Debug.Log("T$$anonymous$$s will be called every frame");
-        if (Input.GetKeyDown("e") && other.tag == "player")
+        if (Keyboard.current[Key.Space].wasPressedThisFrame && other.tag == "player")
         {
+            hp --;
+            if (animator.GetInteger("Hp") == 3)
+            {
+                animator.SetInteger("Hp", 2);
+            }
+            if (animator.GetInteger("Hp") == 2)
+            {
+                animator.SetInteger("Hp", 1);
+            }
+            if (animator.GetInteger("Hp") == 1)
+            {
+                animator.SetInteger("Hp", 0);
+            }
+
             Debug.Log("hit");
         }
     }
